ed96080663fc35d397b9baaa2024372f67e3c1a8
Underprotection <mertcandemirkaya@outlook.com>
2025-07-29 10:39:53 +0300

    Creating Analytics Page

--- app/data/admin/admin-get-recent-courses.ts
+++ app/data/admin/admin-get-recent-courses.ts
@@ -3,27 +3,25 @@ import "server-only";
 import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
-export async function adminGetCourses() {
+export async function adminGetRecentCourses() {
   await requireAdmin();
 
   const data = await prisma.course.findMany({
     orderBy: {
       createdAt: "desc",
     },
+    take: 2,
     select: {
       id: true,
       title: true,
-      slug: true,
       smallDescription: true,
       duration: true,
       level: true,
-      price: true,
       status: true,
       fileKey: true,
+      slug: true,
     },
   });
 
   return data;
 }
-
-export type AdminGetCourseType = Awaited<ReturnType<typeof adminGetCourses>>[0];
